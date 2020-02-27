9d7f94828c73d576718b0eca7214078722eaf226
Tomas Kelečius <45015298+sleepwell95@users.noreply.github.com>
2020-02-27 19:43:36 +0200

    Masked API Key

--- utils/WeatherAPIKey.js
+++ utils/WeatherAPIKey.js
@@ -1,2 +1,2 @@
 // New file for entire API Key. You can secure the file later with the packages if you want to.
-export const API_KEY = '849338767c0e95025b5559533d26b7c4';
+export const API_KEY = 'Your API KEY';
